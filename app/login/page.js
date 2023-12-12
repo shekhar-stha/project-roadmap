333d4576f3b638851232e8ae7d7851868cfb2b24
Shekhar Shrestha <shekharr.shresthaa@gmail.com>
2023-12-12 20:56:29 +0545

    feat: button navigation in login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,10 @@
+"use client"
+
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import Link from 'next/link';
 
 const formFields = [
     { id: 'email', label: 'Email address', type: 'email', placeholder: 'Enter your email' },
@@ -7,12 +12,18 @@ const formFields = [
 ];
 
 export default function LoginPage() {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
     return (
         <section className="login flex items-center justify-center">
-            <div className="login-box shadow-xl border-t-4 border-indigo-500 rounded-lg flex flex-col justify-center px-8 py-12 min-[440px]:px-20 min-[440px]:w-fit w-full mx-auto my-auto">
+            <div className="login-box shadow-xl border-t-4 border-primary rounded-lg flex flex-col justify-center px-8 py-12 min-[440px]:px-20 min-[440px]:w-fit w-full mx-auto my-auto">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <Image src="/logo.png" alt="logo" width={48} height={48} className="mx-auto h-12 w-auto" />
-                    <h2 className="mt-5 text-center text-2xl font-bold leading-9 text-gray-900">
+                    <h2 className="mt-5 text-center text-2xl font-bold leading-9 text-primary">
                         Sign in to your account
                     </h2>
                 </div>
@@ -21,38 +32,50 @@ export default function LoginPage() {
                     <form className="space-y-6" action="#" method="POST">
                         {formFields.map((field) => (
                             <div key={field.id}>
-                                <label htmlFor={field.id} className="block text-base font-medium leading-6 text-gray-900">
-                                    {field.label}
-                                </label>
-                                <div className="mt-2">
-                                    <input
-                                        id={field.id}
-                                        name={field.id}
-                                        type={field.type}
-                                        autoComplete={field.id}
-                                        placeholder={field.placeholder}
-                                        required
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6"
-                                    />
+                                <div>
+                                    <label htmlFor={field.id} className="mb-3 block text-black dark:text-white">
+                                        {field.label}
+                                    </label>
+                                    <div className="relative">
+                                        <input
+                                            id={field.id}
+                                            name={field.id}
+                                            type={passwordVisible ? 'text' : field.type}
+                                            autoComplete={field.id}
+                                            placeholder={field.placeholder}
+                                            className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-large outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                                        />
+                                        {field.type === 'password' && (
+                                            <button
+                                                type="button"
+                                                className="absolute top-1/2 right-4 transform -translate-y-1/2"
+                                                onClick={togglePasswordVisibility}
+                                            >
+                                                {passwordVisible
+                                                    ? <VisibilityIcon className='text-slate-500' />
+                                                    : <VisibilityOffIcon className='text-slate-500' />}
+                                            </button>
+                                        )}
+                                    </div>
                                 </div>
                             </div>
                         ))}
 
                         {/* Submit Button */}
                         <div>
-                            <button
-                                type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Sign in
-                            </button>
+                            <Link
+                            href="/dashboard"
+                                type='submit'
+                                className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray">
+                                Submit
+                            </Link>
                         </div>
                     </form>
 
                     {/* Registration Link */}
                     <p className="mt-10 text-center text-sm text-gray-500">
                         Not registered yet?{' '}
-                        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                        <a href="#" className="font-semibold leading-6 text-primary hover:text-indigo-500">
                             Ask your admin for an invite
                         </a>
                     </p>
